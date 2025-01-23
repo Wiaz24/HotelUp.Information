42c60f97c1bf4e8174cebacb8bd1791001ea2825
Wiaz <wiazhub@gmail.com>
2025-01-23 18:43:19 +0100

    initial migration

---
+++ Src/HotelUp.Information.Persistence/Migrations/20250123174027_Initial.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HotelUp.Information.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "information");
+
+            migrationBuilder.CreateTable(
+                name: "HotelEvents",
+                schema: "information",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlannedDishes",
+                schema: "information",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ImageUrl = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ServingDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlannedDishes", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomInformations",
+                schema: "information",
+                columns: table => new
+                {
+                    Number = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    WithSpecialNeeds = table.Column<bool>(type: "boolean", nullable: false),
+                    Reservations = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomInformations", x => x.Number);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelEvents",
+                schema: "information");
+
+            migrationBuilder.DropTable(
+                name: "PlannedDishes",
+                schema: "information");
+
+            migrationBuilder.DropTable(
+                name: "RoomInformations",
+                schema: "information");
+        }
+    }
+}
