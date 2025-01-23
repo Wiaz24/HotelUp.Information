730a5ac19690d5c02c30587e5b419bd86a551e1c
Wiaz <wiazhub@gmail.com>
2025-01-23 18:25:18 +0100

    Entities

---
+++ Src/HotelUp.Information.Persistence/Entities/HotelEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelUp.Information.Persistence.Entities;
+
+public class HotelEvent
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required DateTime Date { get; set; }
+}
---
+++ Src/HotelUp.Information.Persistence/Entities/RoomReservation.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelUp.Information.Persistence.Entities;
+
+public class RoomReservation
+{
+    public required Guid Id { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
+}
