f1092e2ce1a741220752ade24ff7601f8a3b6aca
Wiaz <wiazhub@gmail.com>
2025-01-24 15:09:57 +0100

    example integration tests

--- Tests/HotelUp.Information.Tests.Integration/IntegrationTestsBase.cs
+++ Tests/HotelUp.Information.Tests.Integration/IntegrationTestsBase.cs
@@ -1,13 +1,19 @@
-﻿namespace HotelUp.Information.Tests.Integration;
+﻿using Xunit.Abstractions;
+
+namespace HotelUp.Information.Tests.Integration;
 
 public abstract class IntegrationTestsBase : IClassFixture<TestWebAppFactory>
 {
     protected TestWebAppFactory Factory { get; }
     protected readonly IServiceProvider ServiceProvider;
+    protected readonly ITestOutputHelper TestOutputHelper;
+    protected readonly string Prefix;
 
-    protected IntegrationTestsBase(TestWebAppFactory factory)
+    protected IntegrationTestsBase(TestWebAppFactory factory, ITestOutputHelper testOutputHelper, string prefix)
     {
         Factory = factory;
+        TestOutputHelper = testOutputHelper;
+        Prefix = prefix;
         ServiceProvider = factory.Services;
     }
 }
