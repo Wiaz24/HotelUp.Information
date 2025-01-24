6f7314f2818846e9e0d4545c000e8304362e5852
Wiaz <wiazhub@gmail.com>
2025-01-24 13:05:20 +0100

    removed memory cache from remaining repositories

--- Src/HotelUp.Information.Persistence/Repositories/PlannedDishRepository.cs
+++ Src/HotelUp.Information.Persistence/Repositories/PlannedDishRepository.cs
@@ -9,43 +9,26 @@ public class PlannedDishRepository : IPlannedDishRepository
 {
     private readonly DbSet<PlannedDish> _plannedDishes;
     private readonly AppDbContext _dbContext;
-    private readonly IMemoryCache _memoryCache;
 
-    public PlannedDishRepository(AppDbContext dbContext, IMemoryCache memoryCache)
+    public PlannedDishRepository(AppDbContext dbContext)
     {
         _plannedDishes = dbContext.Set<PlannedDish>();
         _dbContext = dbContext;
-        _memoryCache = memoryCache;
     }
 
-    public async Task<PlannedDish?> GetByNameAsync(string name)
+    public Task<PlannedDish?> GetByNameAsync(string name)
     {
-        var cacheKey = $"PlannedDish_{name}";
-        var cachedResult = _memoryCache.Get<PlannedDish?>(cacheKey);
-        if (cachedResult is not null)
-        {
-            return cachedResult;
-        }
-        var result = await  _plannedDishes
+        return _plannedDishes
             .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Name == name);
-        _memoryCache.Set(cacheKey, result, TimeSpan.FromMinutes(5));
-        return result;
     }
 
     public async Task<IEnumerable<PlannedDish>> GetByServingDateAsync(DateOnly date)
     {
-        var cacheKey = $"PlannedDish_{date}";
-        var cachedResult = _memoryCache.Get<IEnumerable<PlannedDish>>(cacheKey);
-        if (cachedResult is not null)
-        {
-            return cachedResult;
-        }
         var result = await  _plannedDishes
             .AsNoTracking()
             .Where(x => x.ServingDate == date)
             .ToListAsync();
-        _memoryCache.Set(cacheKey, result, TimeSpan.FromMinutes(5));
         return result;
     }
 
