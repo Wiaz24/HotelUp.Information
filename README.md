eedeaccd18712e062e862ed9f3c7647601bfb61a
Wiaz <wiazhub@gmail.com>
2025-01-25 13:15:11 +0100

    Hotel event integration test

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # HotelUp - Information service
-![dockerhub_badge](https://github.com/Wiaz24/HotelUp.Information/actions/workflows/dockerhub.yml/badge.svg)
+![Application tests](https://github.com/Wiaz24/HotelUp.Information/actions/workflows/tests.yml/badge.svg)
+![Github issues](https://img.shields.io/github/issues/Wiaz24/HotelUp.Information)
+[![Docker Image Size](https://badgen.net/docker/size/wiaz/hotelup.information?icon=docker&label=image%20size)](https://hub.docker.com/r/wiaz/hotelup.information/)
 
 This service should expose endpoints on port `5003` starting with:
 ```http
 /api/information/
 ```
 
-## Healthchecks
+## Health checks
 Health status of the service should be available at:
 ```http
 /api/information/_health
 ```
 and should return 200 OK if the service is running, otherwise 503 Service Unavailable.
+
+## Message broker
+This service uses `MassTransit` library to communicate with the message broker. For the purpose of integration with
+another MassTransit service, all published events are declared in the `HotelUp.Information.Services.Events` namespace.
+
+### AMQP Queues
+This service creates queues that consume messages from the following exchanges:
+- `HotelUp.Customer:RoomCreatedEvent` from [HotelUp.Customer](https://github.com/Wiaz24/HotelUp.Customer)
+- `HotelUp.Customer:ReservationCreatedEvent` from [HotelUp.Customer](https://github.com/Wiaz24/HotelUp.Customer)
+- `HotelUp.Customer:ReservationCanceledEvent` from [HotelUp.Customer](https://github.com/Wiaz24/HotelUp.Customer)
+- `HotelUp.Kitchen:MenuCreatedEvent` from [HotelUp.Kitchen](https://github.com/Wiaz24/HotelUp.Kitchen)
---
+++ Tests/HotelUp.Information.Tests.Integration/EventHandlers/ReservationCreatedEventHandlerTests.cs
@@ -0,0 +1,85 @@
+﻿using HotelUp.Customer.Application.Events;
+using HotelUp.Information.Persistence.Entities;
+using HotelUp.Information.Persistence.Repositories;
+using HotelUp.Information.Services.Events.External.DTOs;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace HotelUp.Information.Tests.Integration.EventHandlers;
+
+[Collection(nameof(ReservationCreatedEventHandlerTests))]
+public class ReservationCreatedEventHandlerTests : IntegrationTestsBase
+{
+    private readonly IBus _bus;
+    public ReservationCreatedEventHandlerTests(TestWebAppFactory factory, ITestOutputHelper testOutputHelper) 
+        : base(factory, testOutputHelper,"")
+    {
+        _bus = ServiceProvider.GetRequiredService<IBus>();
+    }
+    
+    private async Task<IEnumerable<RoomInformation>> GetRoomInformationsAsync(Guid reservationId)
+    {
+        using var scope = ServiceProvider.CreateScope();
+        var roomInformationRepository = scope.ServiceProvider.GetRequiredService<IRoomInformationRepository>();
+        return await roomInformationRepository.GetByReservationIdAsync(reservationId);
+    }
+
+    private async Task<IEnumerable<RoomInformation>> CreateExampleRoomInformationsAsync(int count)
+    {
+        using var scope = ServiceProvider.CreateScope();
+        var roomInformationRepository = scope.ServiceProvider.GetRequiredService<IRoomInformationRepository>();
+        var roomInformations = new List<RoomInformation>();
+        for (var i = 0; i < count; i++)
+        {
+            var roomInformation = new RoomInformation
+            {
+                Number = i+1,
+                Capacity = 1,
+                WithSpecialNeeds = i % 2 == 0,
+                Reservations = []
+            };
+            roomInformations.Add(roomInformation);
+        }
+        await roomInformationRepository.AddRangeAsync(roomInformations);
+        return roomInformations;
+    }
+    
+    [Fact]
+    public async Task Consume_WhenCalled_ShouldAttachReservationToRooms()
+    {
+        await CreateExampleRoomInformationsAsync(3);
+        var exampleImageUrl = "https://example.com/image.jpg";
+        var reservationId = Guid.NewGuid();
+        var reservationCreatedEvent = new ReservationCreatedEvent
+        {
+            ReservationId = reservationId,
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(1),
+            Rooms = new List<RoomDto>
+            {
+                new RoomDto
+                {
+                    Id = 1,
+                    Capacity = 1,
+                    Floor = 1,
+                    ImageUrl = exampleImageUrl,
+                    Type = RoomType.Basic,
+                    WithSpecialNeeds = false
+                },
+            }
+        };
+
+        (await GetRoomInformationsAsync(reservationId)).ShouldBeEmpty();
+        
+        await _bus.Publish(reservationCreatedEvent);
+
+        await Task.Delay(100);
+        
+        var roomInformations = (await GetRoomInformationsAsync(reservationId)).ToList();
+        roomInformations.ShouldNotBeEmpty();
+        roomInformations.Count().ShouldBe(1);
+        roomInformations[0].Number.ShouldBe(1);
+    }
+}
