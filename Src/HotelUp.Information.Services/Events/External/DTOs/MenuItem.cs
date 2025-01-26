45ed2da795fc459c0913bbd0c5d87e94e18f1176
Wiaz <wiazhub@gmail.com>
2025-01-26 21:04:56 +0100

    MenuPublishedEventHandler implemented

---
+++ Src/HotelUp.Information.Services/Events/External/DTOs/MenuItem.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelUp.Information.Services.Events.External.DTOs;
+
+public record MenuItem
+{
+    public required string Name { get; init; }
+    public required string ImageUrl { get; init; }
+}
