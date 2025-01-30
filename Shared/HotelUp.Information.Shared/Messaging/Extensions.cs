047da279d78941f585ea136e8f2c79622241c255
Wiaz <wiazhub@gmail.com>
2025-01-30 11:59:30 +0100

    k8s yaml file changed + endpoint name formatter

--- Shared/HotelUp.Information.Shared/Messaging/Extensions.cs
+++ Shared/HotelUp.Information.Shared/Messaging/Extensions.cs
@@ -15,7 +15,7 @@ internal static class Extensions
         services.AddRabbitMq();
         services.AddMassTransit(busConfigurator =>
         {
-            busConfigurator.SetKebabCaseEndpointNameFormatter();
+            busConfigurator.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter(prefix: "information"));
             if (assembliesWithConsumers.Length > 0)
             {
                 busConfigurator.AddConsumers(assembliesWithConsumers);
